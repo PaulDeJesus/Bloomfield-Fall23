5e85a83aae71c2978ebd014a472b0c9200e0ae2e
PaulDeJesus <paul_dejesus@bloomfield.edu>
2024-02-19 18:34:17 -0500

    3d physics game start

---
+++ BloomfieldFall23/Assets/physicsGame/player3D.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class player3D : MonoBehaviour
+{
+
+    public float speed = .2f;
+    public GameObject myCam;
+    public float camLock = 90;
+    public float lookSpeed = 5f;
+
+    Rigidbody myRB;
+    Vector3 myLook;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        myLook = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerLook = myCam.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position, playerLook * 3f, Color.magenta);
+
+
+        myLook += DeltaLook() * Time.deltaTime;
+
+        if(myLook.y > camLock)
+        {
+            myLook.y = camLock;
+        }
+        else if(myLook.y < -camLock)
+        {
+            myLook.y = -camLock;
+        }
+
+
+        Debug.Log("look dir: " + myLook);
+
+        transform.rotation = Quaternion.Euler(0f, myLook.x, 0f);
+        myCam.transform.rotation = Quaternion.Euler(-myLook.y, myLook.x, 0f);
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 myDir = transform.TransformDirection(Dir());
+        Debug.Log("input dir: " + myDir);
+        myRB.AddForce(myDir * speed);
+    }
+
+
+    Vector3 Dir()
+    {
+        Vector3 moveDir = Vector3.zero;
+
+      /*  if (Input.GetKey(forward))
+        {
+            //moveDir += Vector3.forward + speed;
+        }
+
+       if (Input.GetKey(backwards))
+        {
+
+        }
+
+       if (Input.GetKey(left))
+        {
+
+        }
+
+       if (Input.GetKey(right))
+        {
+
+        }*/
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector3(x, 0, z);
+
+        return moveDir;
+    }
+
+    Vector3 DeltaLook()
+    {
+        Vector3 deltaLook = Vector3.zero;
+        float rotY = Input.GetAxisRaw("Mouse Y") * lookSpeed;
+        float rotX = Input.GetAxisRaw("Mouse X") * lookSpeed;
+
+        deltaLook = new Vector3(rotX, rotY, 0);
+        return deltaLook;
+    }
+
+
+
+}
